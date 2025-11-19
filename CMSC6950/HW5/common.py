20d41c547236c49360b914fb7b51ff8c7b3eb4ca
ieadoboe <ikeadoboe1@gmail.com>
2025-11-18 23:29:52 -0330

    add HW5

---
+++ CMSC6950/HW5/common.py
@@ -0,0 +1,23 @@
+MPL_CONFIG = {
+    'savefig.dpi': 300,  # Publication quality
+    'figure.dpi': 150,   # Screen display
+    'font.family': 'serif',  # Font family
+    'font.size': 11,  # Global font size
+    'font.serif': ['Computer Modern'],
+    'text.usetex': True,  # Enable LaTeX rendering
+    'axes.labelsize': 10,  # Font size for axis labels
+    'axes.titlesize': 11,  # Font size for subplot titles
+    'xtick.labelsize': 9,  # Legend text size
+    'ytick.labelsize': 9,  # Legend text size
+    'legend.fontsize': 10,
+    'lines.linewidth': 2,
+    'axes.linewidth': 0.8,
+    'savefig.bbox': 'tight',
+    'xtick.direction': 'inout',
+    'ytick.direction': 'inout',
+    'xtick.major.width': 0.8,
+    'ytick.major.width': 0.8,
+    'xtick.major.size': 5,
+    'ytick.major.size': 5,
+    'legend.frameon': False
+}
